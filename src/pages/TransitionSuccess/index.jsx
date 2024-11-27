c433fe454b220463ecb4908e551e0222a48a02ed
ashishkumarui6 <ashishkumarui6@gmail.com>
2024-11-27 14:48:57 +0530

    cheack

--- src/pages/TransitionSuccess/index.jsx
+++ src/pages/TransitionSuccess/index.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./index.css";
 import Yes from "../../assets/success.png";
 import MiniBtn from "../../widgets/MiniBtn";
 import { useNavigate } from "react-router-dom";
 import Container from "../../container";
+import { ContextPro } from "../../context";
 const TranstionSuccess = () => {
+  const ctx = useContext(ContextPro);
   const navigate = useNavigate();
 
   const OnSucessClose = () => {
+    localStorage.removeItem("loggedUser");
+    ctx.getToken({});
     navigate("/");
   };
   return (
